2dd8adee3a07a6f18705ecab4b4fd90f83d67721
Ramsey Khadder <ramseykhadder@hotmail.com>
2016-05-02 22:21:17 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,26 @@
+﻿# Getting Started with Microsoft's Bot Framework
+
+Microsoft announced a bot framework at //build/ 2016 to make building chat bots super easy. This tutorial will walk you through
+
+1. [Registering](#Registering) a bot
+1. Creating a simple chat bot
+1. Hosting the bot on Azure
+1. Publishing the bot to Email
+1. Publishing the bot to Web
+1. Publishing the bot to Skype
+1. Publishing the bot to Groupme
+1. Publishing the bot to Slack
+
+
+
+<a name="Registering" />
+# Registering a Bot #
+This section will cover how to register your bot. Registering your bot will allow you to host the logic one place and deploy it to many channels.
+
+1. Navigate to [dev.botframework.com](http://dev.botframework.com) and click on _Sign In_ in the upper right corner.
+1. Click on the _Register a Bot_ tab
+1. Fill out the form
+	1. _Name_: This is the name that is associated with your bot on the bot framework portal and the default name used for its public facing accounts (i.e. it's Skype account).
+	1. _Description_: This is what is visible
+	1. 
+
---
+++ src/server.js
@@ -0,0 +1,67 @@
+"use strict";
+
+// Node modules
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Application vars
+var startIntentRegEx = /play\s+high\s+low/i
+
+// endpoints
+var bot = new builder.BotConnectorBot({appId: 'Squad', appSecret: '91606fa3e08e456a9b22c244c2589cbf'});
+bot.add('/', function(session) {
+
+	var text = session.message.text;
+
+	if (startIntentRegEx.test(text))
+		session.beginDialog('/max-num')
+	else
+		session.sendMessage(null)
+});
+bot.add('/max-num', [
+	function (session) {
+		builder.Prompts.number(session, "What's the max number?")
+	},
+	function (session, results) {
+		var max = results.response;
+		session.userData.max = max;
+		session.userData.num = Math.ceil(Math.random() * max)
+		session.userData.round = 1;
+		session.userData.diff = 0;
+		session.replaceDialog('/round');
+	}
+]);
+bot.add('/round', [
+	function (session) {
+		var msg = "Guess a number.";
+		if (session.userData.diff > 0)
+			msg = "Your guess was too high! Guess again."
+		else if (session.userData.diff < 0)
+			msg = "Your guess was too low! Guess again."
+
+		builder.Prompts.number(session, msg)
+	},
+	function (session, results) {
+		// function vars
+		var round = session.userData.round;
+		var target = session.userData.num;
+		var guess = results.response;
+		
+		// high/low logic
+		if (guess === target) { // Winning Case
+			session.endDialog("Wow you got it in " + round + (round === 1 ? " round" : " rounds"));
+		} else { // Losing case
+			session.userData.diff = guess - target;
+			session.userData.round++;	
+
+			session.replaceDialog("/round");
+		}
+	}
+])
+
+// run server
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3978, function() {
+	console.log('%s listening to %s', server.name, server.url);
+});
